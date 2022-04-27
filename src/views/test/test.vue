01b0c60eb52ff0a0a95dfafe3789c263dc4d7dae
coffeetiger2022@outlook.com <coffeetiger2022@outlook.com>
2022-04-27 15:35:11 +0800

    BUG

--- src/views/test/test.vue
+++ src/views/test/test.vue
@@ -1,15 +1,6 @@
 <template>
 	<div>
-		<p class="test1">Welcome to the OP beta</p>
-		<p class="test2">Welcome to the OP beta</p>
-		<p class="test3">Welcome to the OP beta</p>
-
-
-		<van-collapse v-model="activeNames">
-			<van-collapse-item title="标题1" name="1">
-				代码是写出来给人看的，附带能在机器上运行。代码是写出来给人看的，附带能在机器上运行。代码是写出来给人看的，附带能在机器上运行。代码是写出来给人看的，附带能在机器上运行。代码是写出来给人看的，附带能在机器上运行。代码是写出来给人看的，附带能在机器上运行。代码是写出来给人看的，附带能在机器上运行。代码是写出来给人看的，附带能在机器上运行。
-			</van-collapse-item>
-		</van-collapse>
+		<p class="test1">Hello, OPENPUBLISH</p> 
 
 
 	</div>
